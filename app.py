ee20922b72faffd0fee40566e73b94ec0404bc5a
Kavyasree26 <96222009+Kavyasree26@users.noreply.github.com>
2022-12-25 17:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 app = Flask(__name__)
 
-model = pickle.load(open("model1.pkl", "rb"))
+model =joblib.load(r'models\xg.sav')
 @app.route("/")
 def index():
     return render_template("home.html")
