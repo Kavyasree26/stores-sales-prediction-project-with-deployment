1d9063d3b28ec686e88fbc9cf63bc430fb1b7bc8
Kavyasree26 <96222009+Kavyasree26@users.noreply.github.com>
2022-12-25 16:04:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, render_template, request
 import joblib
 import os
 import numpy as np
-
+import pickle
 app = Flask(__name__)
 
-
+model = pickle.load(open("model.pkl", "rb"))
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -19,8 +19,7 @@ def result():
     item_visibility = float(request.form['item_visibility'])
     item_type = float(request.form['item_type'])
     item_mrp = float(request.form['item_mrp'])
-    outlet_establishment_year = float(
-        request.form['outlet_establishment_year'])
+    outlet_establishment_year = float(request.form['outlet_establishment_year'])
     outlet_size = float(request.form['outlet_size'])
     outlet_location_type = float(request.form['outlet_location_type'])
     outlet_type = float(request.form['outlet_type'])
@@ -28,19 +27,8 @@ def result():
     X = np.array([[item_weight, item_fat_content, item_visibility, item_type, item_mrp,
                  outlet_establishment_year, outlet_size, outlet_location_type, outlet_type]])
 
-    scaler_path = r'models\sc.sav'
-
-    sc = joblib.load(scaler_path)
-
-    X_std = sc.transform(X)
-
-    model_path = r'models\xg.sav'
-
-    model = joblib.load(model_path)
-
-    Y_pred = model.predict(X_std)
-    print(Y_pred)
-    return render_template("home.html",prediction_text=Y_pred)
+    prediction = model.predict(X)
+    return render_template("home.html",prediction_text=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
